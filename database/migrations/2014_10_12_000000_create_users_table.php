0afa493c28e0c26bdeffba4fe6a0fb36483f0eb9
Phạm Đỗ An Tâm <charatsu98@gmail.com>
2020-06-20 14:11:46 +0700

    change user max length

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username',255)->unique();
+            $table->string('username', 64)->unique();
             $table->string('password', 64);
             $table->string('name', 60);
             $table->string('email', 60)->unique();
