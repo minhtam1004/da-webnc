01f528a0eb7f6aafca5b0f2ce7bb8b7f63c97f9d
Phạm Đỗ An Tâm <charatsu98@gmail.com>
2020-07-29 14:28:36 +0700

    change debt list view

--- app/Http/Controllers/DebtController.php
+++ app/Http/Controllers/DebtController.php
@@ -12,6 +12,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Pusher\Pusher;
 
 class DebtController extends Controller
@@ -26,18 +27,49 @@ class DebtController extends Controller
     }
     public function index(Request $request)
     {
+        $validatedData = Validator::make($request->all(), [
+            'limit' => 'numeric|min:1',
+            'page' => 'numeric|min:1',
+            'status' => 'array'
+        ]);
+        if ($validatedData->fails()) {
+            return response()->json(['error' => 'Parameter error'], 422);
+        }
+        $status = ['created','paid','deleted'];
+        if($request->status && array_reduce($request->status, function ($isHas, $num) use($status) {
+            return $isHas || !in_array($num,$status);
+        })){
+            return response()->json(['error' => 'status not include'], 422);
+        };
+        $request->status = $request->status ? $request->status : $status;
         $request->limit = $request->limit ? $request->limit : 10;
         $request->page = $request->page ? $request->page : 1;
+        $request->status = $request->status ? $request->status : [];
         $user = auth('api')->user();
         $acc = $user->account;
-        $debt = $acc->owndebts()->paginate($request->limit, ['*'], 'page', $request->page);
+        $debt = $acc->owndebts()->whereIn('status', $request->status)->paginate($request->limit, ['*'], 'page', $request->page);
         return response()->json($debt, 200);
     }
     public function otherindex(Request $request)
     {
+        $validatedData = Validator::make($request->all(), [
+            'limit' => 'numeric|min:1',
+            'page' => 'numeric|min:1',
+            'status' => 'array'
+        ]);
+        if ($validatedData->fails()) {
+            return response()->json(['error' => 'Parameter error'], 422);
+        }
+        $status = ['created','paid','deleted'];
+        if($request->status && array_reduce($request->status, function ($isHas, $num) use($status) {
+            return $isHas || !in_array($num,$status);
+        })){
+            return response()->json(['error' => 'status not include'], 422);
+        };
+        $request->status = $request->status ? $request->status : $status;
         $request->limit = $request->limit ? $request->limit : 10;
         $request->page = $request->page ? $request->page : 1;
-        $debt = auth('api')->user()->account->otherdebts()->paginate($request->limit, ['*'], 'page', $request->page);;
+        $debt = auth('api')->user()->account->otherdebts()->whereIn('status', $request->status)->paginate($request->limit, ['*'], 'page', $request->page);;
         return response()->json($debt, 200);
     }
 
