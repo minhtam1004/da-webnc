8275238e6eafe270c0bc0b913098d742367eb0bd
minhtam <lehoangminhtam100498@gmail.com>
2020-08-17 22:37:57 +0700

    - Add function manager reminder list

---
+++ resources/js/components/Popup/UpdateReminder.vue
@@ -0,0 +1,188 @@
+<template>
+  <Popup @close-modal="closeAll" title="Thay đổi tên gợi nhớ" minWidth="130vmin">
+    <section id="profile">
+      <mdb-row>
+        <mdb-col md="12">
+          <mdb-card cascade narrow>
+            <mdb-card-body style="height:60vh;overflow: auto">
+              <form data-toggle="validator" role="form" id="register-form">
+                <label for="newName" data-error data-success>Nhập tên gợi nhớ</label>
+                <input
+                  type="text"
+                  id="newName"
+                  name="newName"
+                  v-model="newName"
+                  class="form-control validate"
+                />
+                <br />
+
+                <div class="text-center mt-4">
+                  <button
+                    class="btn btn-unique"
+                    type="button"
+                    id="btn-one"
+                    @click="addToListReminder"
+                  >Xác nhận</button>
+                </div>
+              </form>
+            </mdb-card-body>
+          </mdb-card>
+        </mdb-col>
+      </mdb-row>
+    </section>
+  </Popup>
+</template>
+<script>
+import { mdbRow, mdbCol, mdbCard, mdbView, mdbCardBody, mdbTbl } from "mdbvue";
+import $ from "jquery";
+import validate from "jquery-validation";
+import Popup from "./Popup";
+export default {
+  name: "Profile",
+  components: {
+    mdbRow,
+    mdbCol,
+    mdbCard,
+    mdbView,
+    mdbCardBody,
+    mdbTbl,
+    Popup,
+  },
+  props: {
+    accountId: {
+      type: String,
+      default: "",
+    },
+    bankId: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    id: {
+      type: Number,
+      default: 0,
+    }
+  },
+  data() {
+    return {
+      newName: ""
+    };
+  },
+  created() {
+    this.newName = this.name;
+  },
+  mounted() {
+    $("#password-error").addClass("colorError");
+    $("#register-form").validate({
+      // validation rules for registration form
+      debug: true,
+      errorClass: "active",
+      rules: {
+        name: {
+          required: true,
+          minlength: 1,
+          maxlength: 32,
+        },
+      },
+      messages: {
+        name: {
+          required: "Vui lòng nhập tên gợi nhớ",
+          minlength: "Mật khẩu từ 2 - 32 kí tự",
+          maxlength: "Mật khẩu từ 2 - 32 kí tự",
+        },
+      },
+      submitHandler: function (form) {
+        form.submit();
+      },
+    });
+  },
+  methods: {
+    closeAll() {
+      this.$emit("close-modal");
+    },
+    turnOnLoading() {
+      $("#btn-one")
+        .html(
+          '<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Xác nhận'
+        )
+        .addClass("disabled");
+    },
+    turnOffLoading() {
+      $("#btn-one").removeClass("disabled");
+      $("#btn-one span").remove();
+    },
+
+    addToListReminder() {
+      this.turnOnLoading();
+      var data = {
+        name: this.newName,
+      };
+      const options = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "bearer" + this.$store.state.user.access_token,
+        },
+      };
+      axios
+        // api/remembers/{id}
+        .put("api/bank/remembers/" + this.id, data, options)
+        .then((response) => {
+          console.log("**** ", response);
+          if (response.data !== null) {
+            this.turnOffLoading();
+            this.$toast.open({
+              message: "Đã thêm vào danh sách gợi nhớ",
+              type: "success",
+            });
+
+            this.$emit("close-modal");
+          }
+        })
+        .catch((error) => {
+          console.log("AXIOS ERROR: ", error);
+          this.turnOffLoading();
+
+          this.$toast.open({
+            message: "Thao tác thất bại",
+            type: "success",
+          });
+
+          if (error.response.data.error === "Parameter error") {
+            this.$toast.open({
+              message: "Dữ liệu không hợp lệ vui lòng kiểm tra lại",
+              type: "error",
+            });
+
+            return;
+          }
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+textarea.md-textarea.invalid + label:after {
+  width: 100px;
+  color: crimson !important;
+  top: 115px;
+}
+.profile-card-footer {
+  background-color: #f7f7f7 !important;
+  padding: 1.25rem;
+}
+.card.card-cascade .view {
+  box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.15), 0 3px 12px 0 rgba(0, 0, 0, 0.15);
+}
+.card.card-cascade .view.gradient-card-header {
+  padding: 1rem 1rem;
+  text-align: center;
+}
+.card.card-cascade h3,
+.card.card-cascade h4 {
+  margin-bottom: 0;
+}
+</style>
--- resources/js/components/SearchName.vue
+++ resources/js/components/SearchName.vue
@@ -28,45 +28,75 @@
             placeholder="Nhập tên người nhận"
             aria-label="Search for a country"
             @submit="onSubmit"
-          ></autocomplete>
+          >
+            <template #result="{ result, props }">
+              <li v-bind="props">
+                <div class="wiki-title">
+                  {{ result }}
+                  <button
+                    style="margin-left:28vmin!important"
+                    type="button"
+                    class="btn btn-primary px-4"
+                    @click="update(result)"
+                  >
+                    <i class="fas fa-pencil-alt" aria-hidden="true"></i>
+                  </button>
+                  <button
+                    style="margin-left:1vmin!important"
+                    type="button"
+                    class="btn btn-danger px-4"
+                  >
+                    <i class="fas fa-times-circle" aria-hidden="true"></i>
+                  </button>
+                </div>
+              </li>
+            </template>
+          </autocomplete>
         </div>
         <div class="modal-footer d-flex justify-content-center">
           <button class="btn btn-default" type="button" @click="checkCustomerInfo()">Xác nhận</button>
         </div>
+        <UpdateReminder :name="nameReminder" :id="idReminder" v-if="showAddReminder" @close-modal="showAddReminder = false" />
       </div>
     </div>
   </div>
 </template>
 <script>
 import Autocomplete from "@trevoreyre/autocomplete-vue";
+import UpdateReminder from "./Popup/UpdateReminder";
 export default {
   props: {
     title: {
       tyle: String,
-      default: ""
+      default: "",
     },
     type: {
       tyle: String,
-      default: "success" //danger,info,warning
+      default: "success", //danger,info,warning
     },
     message: {
       tyle: String,
-      default: ""
+      default: "",
     }
   },
   components: {
-    Autocomplete
+    Autocomplete,
+    UpdateReminder,
   },
   data() {
     return {
       isShowing: false,
+      nameReminder: "",
+      idReminder: 0,
+      showAddReminder: false,
       isShowingAfterTransfers: true,
       countries: ["aaaa", "bbbb", "aaa1"],
       users: [],
       reminder: [],
       lastPage: 0,
       accountNumber: 0,
-      bankId: null
+      bankId: null,
+      data: [],
     };
   },
   created() {
@@ -76,16 +106,24 @@ export default {
     closeModal() {
       this.$emit("close-modal", false);
     },
+    update(t) {
+      const index = this.reminder.findIndex((u) => u.name == t);
+      if (index >= 0) {
+        this.showAddReminder = true;
+        this.nameReminder = this.reminder[index].name;
+        this.idReminder = this.reminder[index].id;
+      }
+    },
     getReminderList(page, limit) {
       const options = {
         headers: {
-          Authorization: "bearer" + this.$store.state.user.access_token
-        }
+          Authorization: "bearer" + this.$store.state.user.access_token,
+        },
       };
 
       axios
         .post("api/remembers/search", {}, options)
-        .then(response => {
+        .then((response) => {
           console.log("RESPONSE RECEIVED: ", response);
 
           this.reminder = response.data.data;
@@ -94,7 +132,7 @@ export default {
           }
           this.lastPage = response.data.last_page;
         })
-        .catch(error => {
+        .catch((error) => {
           if (error.response.status === 422) {
             this.$store.dispatch("logOut");
             return;
@@ -103,26 +141,26 @@ export default {
     },
     search(input) {
       if (input.length < 1) {
-        return [];
+        return this.users;
       }
-      return this.users.filter(country => {
+      return this.users.filter((country) => {
         return country.toLowerCase().startsWith(input.toLowerCase());
       });
     },
     onSubmit(result) {
-      const idx = this.reminder.findIndex(u => u.name === result);
+      const idx = this.reminder.findIndex((u) => u.name === result);
       if (idx >= 0) {
         this.accountNumber = this.reminder[idx].accountId;
         this.bankId = this.reminder[idx].bankId;
-      }   
+      }
     },
     checkCustomerInfo() {
       this.$store.dispatch("setAccountNumber", this.accountNumber);
-      console.log("555", this.accountNumber)
+      console.log("555", this.accountNumber);
       this.$emit("user-reminder", this.accountNumber, this.bankId);
       this.closeModal();
-    }
-  }
+    },
+  },
 };
 </script>
 <style scoped>
