055ae3f1951c0bad9e664b12432e365eb2f66004
Phạm Đỗ An Tâm <charatsu98@gmail.com>
2020-08-20 11:07:49 +0700

    fix db

--- database/migrations/2020_05_21_124619_create_transfer_table.php
+++ database/migrations/2020_05_21_124619_create_transfer_table.php
@@ -25,7 +25,7 @@ class CreateTransferTable extends Migration
             $table->string('OTPCode')->nullable();
             $table->boolean('payer')->default(true);
             $table->integer('creator')->nullable()->unsigned();
-            $table->boolean('debtId')->nullable()->default(null);
+            $table->integer('debtId')->nullable()->default(null);
             $table->timestamp('expiresAt')->nullable();
             $table->timestamps();
         });
